c6f7a6a6b44b3959c21a7ea62d121a6f69fa2e83
trvaelmq <329204209@qq.com>
2026-05-30 15:18:38 +0200

    feat: add StaggeredListItem widget

---
+++ lib/widgets/staggered_list_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class StaggeredListItem extends StatefulWidget {
+  final int index;
+  final Widget child;
+
+  const StaggeredListItem({super.key, required this.index, required this.child});
+
+  @override
+  State<StaggeredListItem> createState() => _StaggeredListItemState();
+}
+
+class _StaggeredListItemState extends State<StaggeredListItem>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _ctrl;
+  late final Animation<double> _fade;
+  late final Animation<Offset> _slide;
+
+  @override
+  void initState() {
+    super.initState();
+    _ctrl = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 300));
+    _fade = CurvedAnimation(parent: _ctrl, curve: Curves.easeOut);
+    _slide = Tween<Offset>(
+      begin: const Offset(0, 0.08),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _ctrl, curve: Curves.easeOut));
+
+    Future.delayed(Duration(milliseconds: widget.index * 50), () {
+      if (mounted) _ctrl.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _fade,
+      child: SlideTransition(position: _slide, child: widget.child),
+    );
+  }
+}
