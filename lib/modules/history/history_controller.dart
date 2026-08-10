16c7d0bc0b8348593d323f09fc6093ff4a5ca01e
trvaelmq <329204209@qq.com>
2026-08-10 18:17:36 +0200

    feat(history): HistoryController 改为调用 RiskGateService.fetchAdViews

--- lib/modules/history/history_controller.dart
+++ lib/modules/history/history_controller.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
-import '../../core/services/storage_service.dart';
-import '../../data/models/ad_record.dart';
+import '../../core/services/risk/risk_gate_service.dart';
+import '../../core/services/risk/risk_models.dart';
 
 class HistoryController extends GetxController {
-  final records = <AdRecord>[].obs;
+  final records = <AdViewRecord>[].obs;
+  final isLoading = false.obs;
+  final hasError = false.obs;
 
   @override
   void onInit() {
@@ -11,7 +13,16 @@ class HistoryController extends GetxController {
     loadRecords();
   }
 
-  void loadRecords() => records.value = StorageService.adRecords;
-
-  int get todayCount => StorageService.todayAdRecords.length;
+  Future<void> loadRecords() async {
+    isLoading.value = true;
+    hasError.value = false;
+    try {
+      final result = await RiskGateService.to.fetchAdViews();
+      records.value = result.records;
+    } catch (_) {
+      hasError.value = true;
+    } finally {
+      isLoading.value = false;
+    }
+  }
 }
